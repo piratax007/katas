e709d883a3831d68c1288ca049adae4136274fc9
Fausto <fausto@autonomia.digital>
2021-12-14 02:25:00 +0000

    Clean unnecessary logs

--- langton_ant/langton_ant.py
+++ langton_ant/langton_ant.py
@@ -2,18 +2,20 @@ import board as b
 import ant as a
 
 if __name__ == "__main__":
-    tablero = b.Board(10, 10)
+    tablero = b.Board(5, 5)
     hormiga = a.Ant(0, 0, "N")
 
+    tablero.draw(hormiga)
+
     for _ in range(10):
-        print("--------------------------")
-        print(f"current ant position: ({hormiga.horizontal_position}, {hormiga.vertical_position})")
-        print(f"current ant direction: {hormiga.current_direction}")
-        print(f"current cell state: {tablero.grid[hormiga.vertical_position][hormiga.horizontal_position]}")
-        print("--------------------------")
+        # print("--------------------------")
+        # print(f"current ant position: ({hormiga.horizontal_position}, {hormiga.vertical_position})")
+        # print(f"current ant direction: {hormiga.current_direction}")
+        # print(f"current cell state: {tablero.states[hormiga.vertical_position][hormiga.horizontal_position]}")
+        # print("--------------------------")
         hormiga.move(tablero)
-        tablero.draw()
-        print("--------------------------")
-        print(f"new ant direction: {hormiga.current_direction}")
-        print(f"new ant position: ({hormiga.horizontal_position}, {hormiga.vertical_position})")
-        print("--------------------------")
+        tablero.draw(hormiga)
+        # print("--------------------------")
+        # print(f"new ant direction: {hormiga.current_direction}")
+        # print(f"new ant position: ({hormiga.horizontal_position}, {hormiga.vertical_position})")
+        # print("--------------------------")
