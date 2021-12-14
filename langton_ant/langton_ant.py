65adefb0ea3d3d8cb76fc45b75b90822a8b8a753
Fausto <fausto@autonomia.digital>
2021-12-14 05:48:20 +0000

    Implant refactor in the `Board` object constructor

--- langton_ant/langton_ant.py
+++ langton_ant/langton_ant.py
@@ -1,21 +1,13 @@
 import board as b
 import ant as a
+from random import randint
 
 if __name__ == "__main__":
-    tablero = b.Board(5, 5)
+    tablero = b.Board([[randint(0, 2) for _ in range(5)] for _ in range(5)])
     hormiga = a.Ant(0, 0, "N")
 
     tablero.draw(hormiga)
 
     for _ in range(10):
-        # print("--------------------------")
-        # print(f"current ant position: ({hormiga.horizontal_position}, {hormiga.vertical_position})")
-        # print(f"current ant direction: {hormiga.current_direction}")
-        # print(f"current cell state: {tablero.states[hormiga.vertical_position][hormiga.horizontal_position]}")
-        # print("--------------------------")
         hormiga.move(tablero)
         tablero.draw(hormiga)
-        # print("--------------------------")
-        # print(f"new ant direction: {hormiga.current_direction}")
-        # print(f"new ant position: ({hormiga.horizontal_position}, {hormiga.vertical_position})")
-        # print("--------------------------")
