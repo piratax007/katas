858e7b3f20fdf53e8f93b1c8652f7d27866361c5
Fausto <fausto@autonomia.digital>
2021-12-14 02:23:55 +0000

    Improve the method to print de board

--- langton_ant/board.py
+++ langton_ant/board.py
@@ -6,7 +6,12 @@ class Board:
     def __init__(self, r: int, c: int):
         self.rows = r
         self.columns = c
-        self.grid = [[randint(0, 1) for _ in range(self.columns)] for _ in range(self.rows)]
+        self.states = [[randint(0, 1) for _ in range(self.columns)] for _ in range(self.rows)]
+
+    @staticmethod
+    def printable_board(self, walker: ant):
+        printable_board = [[" _" + str(self.states[i][j]) for j in range(self.columns)] for i in range(self.rows)]
+        printable_board[walker.vertical_position][walker.horizontal_position] = printable_board[walker.vertical_position][walker.horizontal_position].replace("_", walker.current_direction)
 
         return printable_board
 
@@ -16,8 +21,13 @@ class Board:
         elif self.states[walker.vertical_position][walker.horizontal_position] == 0:
             self.states[walker.vertical_position][walker.horizontal_position] = 2
         else:
-            self.grid[walker.vertical_position][walker.horizontal_position] = 1
+            self.states[walker.vertical_position][walker.horizontal_position] = 1
 
-    def draw(self):
-        for row in range(self.rows):
-            print(self.grid[row], sep="\n")
+        self.printable_board(self, walker)
+
+    def draw(self, hormiga: ant):
+        align_factor = len(str(self.rows))
+
+        print(" " * align_factor + "-" * (self.columns * 3 + 3))
+        print(*[f"{i:>{align_factor}}|" + "".join(self.printable_board(self, hormiga)[i]) + " |" for i in range(self.rows)], sep='\n')
+        print(" " * align_factor + "-" * (self.columns * 3 + 3))
