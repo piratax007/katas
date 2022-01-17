e42f7ab0e0cedbc14cf22a5218b27f922d037ba6
Fausto <fausto@autonomia.digital>
2022-01-17 05:50:59 +0000

    update code

---
+++ roman_numerals/roman_numerals_convert.py
@@ -0,0 +1,61 @@
+def validate_input(n: int) -> str:
+    if isinstance(n, float) or not (0 < n < 4000):
+        raise ValueError("The input must be a positive integer")
+
+    return str(n)
+
+
+def separate_into_digits(int_as_str: str) -> list:
+    list_of_digits = []
+
+    for _, digit in enumerate(int_as_str):
+        list_of_digits.append(int(digit))
+
+    return list_of_digits
+
+
+def assign_symbol(val: int, position: str) -> str:
+    pivots = {
+        "units": "I",
+        "half_tens": "V",
+        "tens": "X",
+        "half_hundreds": "L",
+        "hundreds": "C",
+        "half_thousands": "D",
+        "thousands": "M",
+    }
+
+    next_pivot = {"units": "tens", "tens": "hundreds", "hundreds": "thousands"}
+
+    if val < 4:
+        return pivots[position] * val
+
+    if val == 4:
+        return pivots[position] + pivots["half_" + next_pivot[position]]
+
+    if val == 5:
+        return pivots["half_" + next_pivot[position]]
+    
+    if 5 < val < 9:
+        return pivots["half_" + next_pivot[position]] + pivots[position] * (val - 5)
+
+    if val == 9:
+        return pivots[position] + pivots[next_pivot[position]]
+
+
+def arabic_to_roman(arabic_int: int) -> str:
+    dec_int = separate_into_digits(validate_input(arabic_int))
+
+    if len(dec_int) == 1:
+        return assign_symbol(dec_int[0], "units")
+
+    if len(dec_int) == 2:
+        return assign_symbol(dec_int[0], "tens") + assign_symbol(dec_int[1], "units")
+
+    if len(dec_int) == 3:
+        return assign_symbol(dec_int[0], "hundreds") + assign_symbol(dec_int[1], "tens") + \
+               assign_symbol(dec_int[2], "units")
+
+    if len(dec_int) == 4:
+        return assign_symbol(dec_int[0], "thousands") + assign_symbol(dec_int[1], "hundreds") + \
+               assign_symbol(dec_int[2], "tens") + assign_symbol(dec_int[3], "units")
---
+++ roman_numerals/test_roman_numerals_convert.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+
+import roman_numerals_convert as rmc
+
+
+class Test(TestCase):
+    def test_verify_that_the_input_number_is_a_positive_integer(self):
+        with self.assertRaises(ValueError):
+            rmc.validate_input(-10)
+            rmc.validate_input(3.141592)
+            rmc.validate_input(4000)
+
+    def test_convert_the_input_integer_to_a_string(self):
+        self.assertEqual("1234", rmc.validate_input(1234))
+
+    def test_separate_the_input_number_into_a_list_of_digits(self):
+        self.assertListEqual([1, 2, 3, 4], rmc.separate_into_digits("1234"))
+
+    def test_convert_an_arabic_integer_to_roman_numeral(self):
+        self.assertEqual("I", rmc.arabic_to_roman(1))
+        self.assertEqual("V", rmc.arabic_to_roman(5))
+        self.assertEqual("VII", rmc.arabic_to_roman(7))
+        self.assertEqual("IX", rmc.arabic_to_roman(9))
+        self.assertEqual("XXIII", rmc.arabic_to_roman(23))
+        self.assertEqual("XXV", rmc.arabic_to_roman(25))
+        self.assertEqual("XXIX", rmc.arabic_to_roman(29))
+        self.assertEqual("XLV", rmc.arabic_to_roman(45))
+        self.assertEqual("LXXXIX", rmc.arabic_to_roman(89))
+        self.assertEqual("C", rmc.arabic_to_roman(100))
+        self.assertEqual("DLXXXII", rmc.arabic_to_roman(582))
+        self.assertEqual("MDCCLXXVI", rmc.arabic_to_roman(1776))
+        self.assertEqual("MMCDXXI", rmc.arabic_to_roman(2421))
+        self.assertEqual("MMMCMXCIX", rmc.arabic_to_roman(3999))
