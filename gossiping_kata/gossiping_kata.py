9d7447286dd252512e4c1d96c97c4f423d205f4e
Fausto M. Lagos S <fausto@autonomia.digital>
2022-03-31 00:12:05 -0500

    Refactoring and improving naming

--- gossiping_kata/gossiping_kata.py
+++ gossiping_kata/gossiping_kata.py
@@ -1,50 +1,45 @@
-class simulation():
+class Simulation:
     def __init__(self, drivers: list):
         if len(drivers) < 2:
             raise Exception("with less than two drivers can't be perform a simulation")
 
         self.drivers = drivers
 
-    def setup_current_stop(self, step: int):
+    def setup_current_stop(self, minute: int):
         for driver in self.drivers:
-            if step >= len(driver.route):
-                driver.current_stop = driver.route[step % len(driver.route)]
-            else:
-                driver.current_stop = driver.route[step]
+            driver.current_stop = driver.route[minute % len(driver.route)]
 
-    def drivers_in_same_stop(self, driver, step: int) -> list:
-        rest_drivers = self.drivers.copy()
-        rest_drivers.remove(driver)
-        self.setup_current_stop(step)
-        return [d for d in filter(lambda d: d.current_stop == driver.current_stop, rest_drivers)]
+    def rest_drivers(self, driver) -> list:
+        rd = self.drivers.copy()
+        rd.remove(driver)
+        return rd
+
+    @staticmethod
+    def filter_drivers_by_stop(stop: int, drivers_to_filter: list) -> list:
+        return list(filter(lambda d: d.current_stop == stop, drivers_to_filter))
+
+    def drivers_at_same_stop(self, driver, minute: int) -> list:
+        self.setup_current_stop(minute)
+        rd = self.rest_drivers(driver)
+        return self.filter_drivers_by_stop(driver.current_stop, rd)
+
+    def exchange_gossips(self, driver, minute):
+        for d in self.drivers_at_same_stop(driver, minute):
+            driver.add_unknown_gossips(d)
+            d.add_unknown_gossips(driver)
+
+    def has_all_drivers_all_gossips(self) -> bool:
+        return all(len(d.known_gossips) == len(self.drivers) for d in self.drivers)
 
     def execute(self) -> int:
-        step = 0
+        minute = 0
         while True:
             for driver in self.drivers:
-                coincidents = self.drivers_in_same_stop(driver, step)
-                if coincidents:
-                    for d in coincidents:
-                        driver.update_gossips(d)
-                        d.update_gossips(driver)
-                else:
-                    continue
+                self.exchange_gossips(driver, minute)
 
-            drivers_with_all_gossips = all(len(d.known_gossips) == len(self.drivers) for d in self.drivers)
-
-            if drivers_with_all_gossips:
-                return step + 1
-            elif step < 481:
-                step += 1
+            if self.has_all_drivers_all_gossips():
+                return minute + 1
+            elif minute < 481:
+                minute += 1
             else:
                 return -1
-
-
-
-# ToDo
-# - Un objeto driver que tenga la lista de gossips conocidos ✓
-# - Un método del objeto driver que intercambie los chismes ✓
-# - Un método del objeto simulation que determine qué parada debe leerse en los drivers en un paso dado de la simulación ✓
-# - Un método del objeto simulation que determine cuándo dos o más drivers coinciden en la misma parada ✓
-# - Un método del objeto simulación que ejecute la simulación hasta que todos los drivers conozcan todos
-#   los chismes o hasta determinal que nunca sucederá ✓
