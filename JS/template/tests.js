c95c75c0e9a96807401f6bb568dd025fdcc960ac
Fausto M. Lagos S <fausto@autonomia.digital>
2023-03-10 12:22:34 -0500

    Improved typo in JS test file

--- JS/template/tests.js
+++ JS/template/tests.js
@@ -3,10 +3,10 @@ import { expect } from "chai";
 import { functions } from "./code";
 
 describe("test suit description", () => {
-    describe("test description"), () => {
+    describe("test description", () => {
 	it("dummy test", () => {
             let variable = 3;
             expect(variable).to.equal(2);
 	});
-    }
+    });
 });
