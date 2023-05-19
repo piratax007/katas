3a4e16ebad3facb15fc6f5a61f7cc32adc8271bf
Fausto M. Lagos S <fausto@autonomia.digital>
2023-05-18 22:17:19 -0500

    Set the environment

---
+++ go/potter_kata/potter_kata.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func dummy() {
+	fmt.Println("This is a dummy function")
+}
+
+func main() {
+	dummy()
+}
---
+++ go/potter_kata/potter_kata_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type kataSuite struct {
+	suite.Suite
+	tdir string
+}
+
+func TestSuite(t *testing.T) {
+	suite.Run(t, new(kataSuite))
+}
+
+func (s *kataSuite) SetupTest() {
+	s.tdir = s.T().TempDir()
+}
+
+func (s *kataSuite) Test_functionsName_testDescription() {}
